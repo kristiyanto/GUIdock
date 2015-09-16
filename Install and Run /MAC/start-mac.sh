324f1047ad8cb852b7237ac5120cf4cd8f78742d
Daniel Kristiyanto <danielkr@uw.edu>
2015-09-16 13:04:07 -0700

    start-mac

--- Install and Run /MAC/start-mac.sh
+++ Install and Run /MAC/start-mac.sh
@@ -8,15 +8,6 @@
 
 export MacIP="192.168.99.1"
 
-# Look for available Docker Machine.
-
-if [$(docker-machine ls | awk 'FNR == 5 {print $1}') eq ""]
-	then
-	echo "Docker Machine is not found. Please check if Docker is properly Installed."
-	echo "Script will now exit."
-	exit 1
-fi
-
 
 # This script works with other Docker Images.
 if [ $# -eq 0 ]
@@ -26,13 +17,26 @@ if [ $# -eq 0 ]
 	export DockerImage=$1
 fi
 
-echo launching $DockerImage on $(docker-machine ls | awk 'FNR == 2 {print $1}')
+
+# Look for available Docker Machine.
+if ["$(docker-machine ls | awk 'FNR == 2 {print $1}')" eq ""] 
+	then
+	echo "Docker Machine is not found. Please check if Docker is properly Installed."
+	echo "Script will now exit."
+	exit 1
+	else
+	echo launching $DockerImage on $(docker-machine ls | awk 'FNR == 2 {print $1}')
+fi
+
 # Script starts here ---
 docker-machine start $(docker-machine ls | awk 'FNR == 2 {print $1}')
 echo "Launching Socat to bind the X11 services."
+
 socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\" &
 echo "Launching XQuartz"
+
 open -a xquartz &
+echo "Setting up environtment"
 eval $(docker-machine env $(docker-machine ls | awk 'FNR == 2 {print $1}'))
 docker pull $DockerImage
 docker run -ti -e DISPLAY=$MacIP:0 $DockerImage
